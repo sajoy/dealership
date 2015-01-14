5e559b59f15ebd6050dbf8d0110527a31f18415a
Sarah Joy Calpo & Jacqueline McVey <student@epicodus.com>
2015-01-14 12:10:05 -0800

    Add dealership file.

--- app.rb
+++ app.rb
@@ -3,9 +3,17 @@ require('sinatra/reloader')
 also_reload('/lib/**/*.rb')
 
 require('./lib/vehicle')
+require('./lib/dealership')
 
 get('/') do
-  @vehicles = Vehicle.all()
+  @dealerships = Dealership.all()
+  erb(:index)
+end
+
+post('/dealership') do
+  name = params.fetch("name")
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
   erb(:index)
 end
 
@@ -13,13 +21,20 @@ post('/vehicle') do
   make = params.fetch("make")
   model = params.fetch("model")
   year = params.fetch("year")
-  vehicle = Vehicle.new(make, model, year)
-  vehicle.save()
-  @vehicles = Vehicle.all()
-  erb(:index)
+  @vehicle = Vehicle.new(make, model, year)
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:dealership)
 end
 
 get('/vehicles/:id') do
   @vehicle = Vehicle.find(params.fetch("id"))
-  erb(:vehicle)  
+  erb(:vehicle)
+end
+
+
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch("id").to_i())
+  erb(:dealership)
 end

ce473f6ffc55c5b9942cc42dbbcdcaed32126ff8
Sarah Joy Calpo & Jacqueline McVey <student@epicodus.com>
2015-01-14 11:39:08 -0800

    add specs and method, plus Sinatra app

---
+++ app.rb
@@ -0,0 +1,25 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('/lib/**/*.rb')
+
+require('./lib/vehicle')
+
+get('/') do
+  @vehicles = Vehicle.all()
+  erb(:index)
+end
+
+post('/vehicle') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  @vehicles = Vehicle.all()
+  erb(:index)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id"))
+  erb(:vehicle)  
+end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -30,6 +30,14 @@ describe(Vehicle) do
     end
   end
 
+  describe("#id") do
+    it("returns the unique id of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.id()).to(eq(1))
+    end
+  end
+
  describe("#save") do
    it("saves the given car to an array") do
      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -52,4 +60,27 @@ describe(Vehicle) do
    end
  end
 
+  describe("age") do
+    it("returns the vehicle's age") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.age()).to(eq(15))
+    end
+  end
+
+  describe("worth_buying?") do
+    it("returns false if the car is not American made and less than 15 years old") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.worth_buying?()).to(eq(false))
+    end
+  end
+
+  describe(".find") do
+    it("returns a vehicle by its id number") do
+      test_vehicle = Vehicle.new("Toyota","Prius", 2000)
+      test_vehicle.save()
+      test_vehicle2 = Vehicle.new("Toyota", "Corolla", 1999)
+      test_vehicle2.save()
+      expect(Vehicle.find(test_vehicle2.id())).to(eq(test_vehicle2))
+    end
+  end
 end
